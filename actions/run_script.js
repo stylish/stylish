5207f718f0662b08ae5f349aa2fb8e62ab2fc289
Jonathan Soeder <jonathan.soeder@gmail.com>
2016-02-08 16:31:36 -0600

    making sure each package has at least minimal test capabilities

--- actions/run_script.js
+++ actions/run_script.js
@@ -10,7 +10,8 @@ cli(function (program, dispatch){
     .description('ensures that a project document exists for each project')
     .option('--packages-only', 'only include packages')
     .option('--projects-only', 'only include projects')
-    .option('--exclude <pattern>','exclude pattern')
+    .option('--exclude <list>','exclude a comma separated list of package names')
+    .option('--include <list>','include a comma separated list of package names')
     .action(dispatch(action.api.runner))
 
   return program
@@ -19,14 +20,43 @@ cli(function (program, dispatch){
 execute(function(scriptName, params, context) {
   let shell = require('shelljs')
   let project = context.project
+  let {isArray, isString, trim, some} = require('lodash')
+
+  if (params.include && isString(params.include)) {
+    if(isString(params.include) && params.include.match(',')) {
+      params.include = params.include.split(',').map(i => trim(i))
+    }
+  }
+
+  if (params.include && !isArray(params.include)){
+    params.include = [params.include]
+  }
+
+  if (params.exclude && isString(params.exclude)) {
+    if(isString(params.exclude) && params.exclude.match(',')) {
+      params.exclude = params.exclude.split(',').map(i => trim(i))
+    }
+  }
+
+  if (params.exclude && !isArray(params.exclude)){
+    params.exclude = [params.exclude]
+  }
 
   let packages = project.packages.query((pkg) => {
     if(!params.exclude && !params.include) {
       return true
     }
 
-    if (params.exclude && pkg.data && pkg.data.name && pkg.data.name.match(params.exclude)) {
-      return false
+    if (params.include) {
+      return some(params.include, (inc) => {
+        return pkg.result('name') === inc
+      })
+    }
+
+    if (params.exclude) {
+      return !some(params.exclude, (inc) => {
+        return pkg.result('name') === inc
+      })
     }
 
     return true
@@ -58,7 +88,7 @@ execute(function(scriptName, params, context) {
       }
 
       if (code !== 0) {
-        console.log(`${ pkg.data.name } ...` + 'ok'.red)
+        console.log(`${ pkg.data.name } ...` + 'not ok'.red)
       }
     })
 
