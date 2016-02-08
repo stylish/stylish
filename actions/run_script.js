94bbff922544c02ccf0e7336fa6a5e58793c7a82
Jonathan Soeder <jonathan.soeder@gmail.com>
2016-02-07 22:59:29 -0600

    version bumping all the scripts; implemented action to run all test suites

--- actions/run_script.js
+++ actions/run_script.js
@@ -16,13 +16,52 @@ cli(function (program, dispatch){
   return program
 })
 
-execute(function(params, context) {
+execute(function(scriptName, params, context) {
+  let shell = require('shelljs')
   let project = context.project
 
-  forEachPackage(project, (pkg) => {
-    if (pkg.sourcePath && pkg.manifestPath) {
-      console.log('Package: ', pkg.name, Object.keys(pkg.readManifest().scripts || {}))
+  let packages = project.packages.query((pkg) => {
+    if(!params.exclude && !params.include) {
+      return true
     }
+
+    if (params.exclude && pkg.data && pkg.data.name && pkg.data.name.match(params.exclude)) {
+      return false
+    }
+
+    return true
+  })
+
+  packages.forEach((pkg) => {
+    if (typeof pkg.result('scripts.test') === 'undefined') {
+      console.log(`${ pkg.name } does not have the ${ scriptName } script`)
+      return
+    }
+
+    let proc = require('child_process').spawn('npm', ['run', scriptName], {
+      cwd: require('path').dirname(
+        pkg.paths.absolute
+      )
+    })
+
+    if (params.stdout) {
+      proc.stdout.on('data', (data) => console.log(data.toString()))
+    }
+
+    if (params.stderr) {
+      proc.stderr.on('data', (data) => console.log(data.toString()))
+    }
+
+    proc.on('exit', (code) => {
+      if ( code == 0 ) {
+        console.log(`${ pkg.data.name } ...` + 'ok'.green)
+      }
+
+      if (code !== 0) {
+        console.log(`${ pkg.data.name } ...` + 'ok'.red)
+      }
+    })
+
   })
 })
 
