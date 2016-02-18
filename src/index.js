977bf69adfb7e82f5da9000bb51f29364e988906
Jonathan Soeder <jonathan.soeder@gmail.com>
2016-02-18 00:08:48 -0600

    relinking skypager-project

--- src/index.js
+++ src/index.js
@@ -1,13 +1,1 @@
-import { Application } from 'ui/applications'
-import DefaultLayout from 'ui/layouts/DefaultLayout'
-import HomePage from './entries/HomePage'
-
-Application.create({
-  root: 'app',
-  layout: DefaultLayout,
-  entryPoints:{
-    index: {
-      component: HomePage
-    }
-  }
-})
+module.exports = require('skypager-project')
