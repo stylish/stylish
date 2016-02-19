323f706f0af416d584ec8b9b857878c4074897db
Jonathan Soeder <jonathan.soeder@gmail.com>
2016-02-19 05:29:07 -0600

    cleaning up actions; adding package docs

--- actions/index.js
+++ actions/index.js
@@ -4,19 +4,6 @@ module.exports = function LoadsActions (actions) {
   )
 
   actions.load(
-    require('./sync_versions'), { id: 'sync_versions', uri: require.resolve('./sync_versions') }
-  )
-
-  actions.load(
     require('./run_script'), { id: 'run_script', uri: require.resolve('./run_script') }
   )
-
-  actions.load(
-    require('./link_packages'), { id: 'link_packages', uri: require.resolve('./link_packages') }
-  )
-
-  actions.load(
-    require('./publish_packages.js'), { id: 'publish_packages', uri: require.resolve('./publish_packages') }
-  )
-
 }
