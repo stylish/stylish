3a75d970bc359d1477644da98aa45093e8a1d9b7
Jonathan Soeder <jonathan.soeder@gmail.com>
2016-02-25 18:05:29 -0600

    adding prepbublish scripts

--- packages/skypager-cli/bin/cli.js
+++ packages/skypager-cli/bin/cli.js
@@ -1,8 +1,21 @@
 #!/usr/bin/env node
 
+var join = require('path').join
+
+require('../lib/dependencies').checkAll()
+
+$skypager['skypager-cli'] = $skypager.cli = require('../lib/util').findPackageSync('skypager-cli') || join(__dirname, '..')
+
 if (process.env.SKYPAGER_ENV === 'development') {
   require('babel-register')
-  require('../src').cli()
+
+  require(
+    join($skypager.cli, 'src')
+  ).cli()
 } else {
-  require('../lib').cli()
+  require('babel-register')
+
+  require(
+    join($skypager.cli, 'lib')
+  ).cli()
 }
