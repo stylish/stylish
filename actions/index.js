60a3dfd7ac36d6d65594d674aa2212e090dba238
Jonathan Soeder <jonathan.soeder@gmail.com>
2016-03-13 14:45:08 -0500

    Remove devpack dependencies from skypager-ui
    Skypager devpack should contain all of the webpack related build tools
    needed at development time. Skypager UI contains all of the react redux
    dependencies, etc.

--- actions/index.js
+++
@@ -1,8 +0,0 @@
-module.exports = function LoadsActions(actions) {
-  actions.load(
-    require('./vectors/reactify.js'),{
-      id: 'svg/reactify',
-      uri: require.resolve('./vectors/reactify.js')
-    }
-  )
-}
